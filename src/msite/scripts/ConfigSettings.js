96472c4c7c1518d2e0ecaed681cf9d7e3113145d
Chris Hatton <chris.hatton@gmail.com>
2010-08-13 14:17:03 -0600

    Added missing ConfigSettings.js file

---
+++ src/msite/scripts/ConfigSettings.js
@@ -0,0 +1,13 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+
+</head><body>var SERVICE_DOMAIN_PROXY = "apiproxy";
+
+var configSettings = {};
+
+configSettings.clientstring = "";
+configSettings.apiproxy =  "https://m-api.ecollege.com";
+configSettings.wmm = {};
+configSettings.wmm.enabled = "true"; 
+</body></html>
