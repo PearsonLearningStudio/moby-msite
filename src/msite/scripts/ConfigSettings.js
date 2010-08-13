e63a69309f4bf2d2481fb8c1d8a73a557c87c339
matt-eclg <matts@ecollege.com>
2010-08-13 14:24:35 -0700

    removed html tags

--- src/msite/scripts/ConfigSettings.js
+++ src/msite/scripts/ConfigSettings.js
@@ -1,13 +1,8 @@
-<html><head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-
-
-</head><body>var SERVICE_DOMAIN_PROXY = "apiproxy";
+var SERVICE_DOMAIN_PROXY = "apiproxy";
 
 var configSettings = {};
 
 configSettings.clientstring = "";
 configSettings.apiproxy =  "https://m-api.ecollege.com";
 configSettings.wmm = {};
-configSettings.wmm.enabled = "true"; 
-</body></html>
+configSettings.wmm.enabled = "true";
